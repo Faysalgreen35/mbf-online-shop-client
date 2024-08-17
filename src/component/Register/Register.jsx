c520f91ca3a21bc3e1d403465d7e512f554b7c09
MusnudBillahFaysal <138293528+MusnudBillahFaysal@users.noreply.github.com>
2024-08-17 17:50:27 +0600

    register  modify  commit

--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -84,11 +84,21 @@ const Register = () => {
     return (
         <>
             <Helmet>
-                <title>Pet Adoption | Sign Up</title>
+                <title>  | Sign Up</title>
             </Helmet>
             <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
-                <div className="hidden bg-cover lg:block lg:w-1/2" style={{ backgroundImage: "url('https://img.freepik.com/premium-vector/geometric-gradient-technology-background_23-2149110132.jpg')" }}>
+            <div className="hidden  lg:bg-cover lg:block lg:w-1/2" style={{ backgroundImage: "url('https://img.freepik.com/premium-vector/geometric-gradient-technology-background_23-2149110132.jpg')" }}>
+                <p className="mt-3 text-sm md:text-2xl  text-center text-gray-100 dark:text-gray-200 font-bold font-mono">
+                    <span className="text-center mx-auto font-bold font-mono">
+                        <img className="text-center  mx-auto mt-3 md:mt-12 mb-3" src={logo} alt="" />
+                    </span>
+                      <span className="mt-12">
+                      Welcome to MBF Shop
+                      </span>
+                    </p>
+
                 </div>
+
 
                 <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
                     <div className="flex justify-center mx-auto">
@@ -96,7 +106,7 @@ const Register = () => {
                     </div>
 
                     <p className="mt-3 text-xl text-center text-gray-600 dark:text-gray-200">
-                        Welcome  To Sign Up
+                        Welcome  To  Register page
                     </p>
                   {/* // social  */}
                   <SocialLogin></SocialLogin>
